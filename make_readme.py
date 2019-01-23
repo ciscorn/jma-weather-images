e10eca16b001207fd66fa34e90da2322e62e2d1a
Taku Fukada <naninunenor@gmail.com>
2019-01-24 03:25:56 +0900

    Add JMA images

--- make_readme.py
+++ make_readme.py
@@ -4,8 +4,8 @@ import json
 with open("codes.json", encoding="utf-8") as f:
     data = json.load(f)
 
-print('|code|description|instruction|fkd|jtj|')
-print('|:-:|---|:-:|:-:|')
+print('|code|description|instruction|jma|fkd|jtj|')
+print('|:-:|---|---|:-:|:-:|:-:|')
 
 for key in sorted(data.keys()):
     d = data[key]
@@ -13,4 +13,5 @@ for key in sorted(data.keys()):
     m = d.get("m", "")
     t = d.get("t", "")
     ja = d.get("desc", "")
-    print(f'| {key} | {ja} | {b} {m} {t} | <img width="50" src="./output_refs/{key}.png" /> | <img width="50" src="./output/{key}.png" /> |')
+    print(f'| {key} | {ja} | {b} {m} {t} | <img width="50" src="./jma/{key}.png" /> | <img width="50" src="./output_refs/{key}.png" /> | <img width="50" src="./output/{key}.png" /> |')
+
